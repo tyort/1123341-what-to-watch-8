ff1c4849f1083cd7e9c359c636a6ad150331ce34
tyort <neotyo1986@gmail.com>
2021-12-05 19:33:31 +0300

    Подключение store в index.tsx

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
-// для работы с web. Вместо него для разработки мобильных приложений
-// можно использовать react-native
-import ReactDOM from 'react-dom';
-
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {reducer} from './store/reducer'; // на основании action меняет state в store.
+import ReactDOM from 'react-dom'; // для работы с web. Вместо него для разработки мобильных приложений можно использовать react-native.
 import App from './components/app/app';
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(), // Добавит поддержку инструмента Redux DevTools (расширение для браузера).
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Provider - делает "Redux store" доступным для вызовов connect() из всех дочерних компонентов.*/}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
