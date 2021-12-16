099578479822e5cbd78f8e769ee2c5065ebf3515
tyort <neotyo1986@gmail.com>
2021-12-16 14:43:51 +0300

    Обращение к несуществующей странице.

--- project/src/components/not-found/not-found.tsx
+++ project/src/components/not-found/not-found.tsx
@@ -1,10 +1,11 @@
 import {Link} from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function NotFoundScreen(): JSX.Element {
   return (
     <section>
       <h1>404. Page not found</h1>
-      <Link to="/">Вернуться на главную</Link>
+      <Link to={AppRoute.Main}>Вернуться на главную</Link>
     </section>
   );
 }
