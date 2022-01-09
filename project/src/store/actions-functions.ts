e9fcea7e4e2b2fa51ba025b714f90925c3e1a7b0
tyort <neotyo1986@gmail.com>
2022-01-09 20:28:18 +0300

    Рефакторим функции(создателей действий), посредством ф-ии createAction(@reduxjs/toolkit)

--- project/src/store/actions-functions.ts
+++ project/src/store/actions-functions.ts
@@ -5,65 +5,62 @@ import { Comment } from '../types/comment';
 import { AuthInfo } from '../types/user';
 import { createAction } from '@reduxjs/toolkit';
 
-// Функция возращает объект(действие)
-export const changeGenre = (genre: string) => ({
-  type: ActionName.ChangeGenre,
-  payload: genre,
-// Объект, который возвращает функция changeGenre, является константой.
-// Таким образом мы можем не типизировать возвращаемый результат(объект).
-} as const);
+export const changeGenre = createAction(
+  ActionName.ChangeGenre,
+  (genre: string) => ({payload: genre}),
+);
 
 // increaseMoviesCount - функция(создатель действия)
 // Особенность: increaseMoviesCount.toString() === 'movies/increaseCount'
 export const increaseMoviesCount = createAction(ActionName.IncreaseCount);
 
-export const defaultMoviesCount = () => ({
-  type: ActionName.DefaultMoviesCount,
-} as const);
+export const defaultMoviesCount = createAction(ActionName.DefaultMoviesCount);
 
-export const loadMovies = createAction(ActionName.LoadMovies, (movies: Movie[]) => ({
-  payload: movies,
-  // currentTime: new Date().getTime(), --> новое поле, просто как пример.
-}));
+export const loadMovies = createAction(
+  ActionName.LoadMovies,
+  (movies: Movie[]) => ({
+    payload: movies,
+    // currentTime: new Date().getTime(), --> новое поле, просто как пример.
+  }),
+);
 
-export const loadFavorites = (movies: Movie[]) => ({
-  type: ActionName.LoadFavorites,
-  payload: movies,
-} as const);
+export const loadFavorites = createAction(
+  ActionName.LoadFavorites,
+  (movies: Movie[]) => ({payload: movies}),
+);
 
-export const loadSimilar = (movies: Movie[]) => ({
-  type: ActionName.LoadSimilar,
-  payload: movies,
-} as const);
+export const loadSimilar = createAction(
+  ActionName.LoadSimilar,
+  (movies: Movie[]) => ({payload: movies}),
+);
 
-export const loadPromo = (movie: Movie) => ({
-  type: ActionName.LoadPromo,
-  payload: movie,
-} as const);
+export const loadPromo = createAction(
+  ActionName.LoadPromo,
+  (movie: Movie) => ({payload: movie}),
+);
 
-export const loadComments = (comments: Comment[]) => ({
-  type: ActionName.LoadComments,
-  payload: comments,
-} as const);
+export const loadComments = createAction(
+  ActionName.LoadComments,
+  (comments: Comment[]) => ({payload: comments}),
+);
 
-export const setAuthStatus = (authStatus: AuthorizationStatus) => ({
-  type: ActionName.SetAuthStatus,
-  payload: authStatus,
-} as const);
+export const setAuthStatus = createAction(
+  ActionName.SetAuthStatus,
+  (authStatus: AuthorizationStatus) => ({payload: authStatus}),
+);
 
-export const loadDataUser = (userData: AuthInfo | null) => ({
-  type: ActionName.LoadDataUser,
-  payload: userData,
-} as const);
+export const loadDataUser = createAction(
+  ActionName.LoadDataUser,
+  (userData: AuthInfo | null) => ({payload: userData}),
+);
 
-export const failPostComment = (isFailed: boolean) => ({
-  type: ActionName.FailPostComment,
-  payload: isFailed,
-} as const);
+export const failPostComment = createAction(
+  ActionName.FailPostComment,
+  (isFailed: boolean) => ({payload: isFailed}),
+);
 
 // Это действие для middleware, там мы его связываем с объектом "history";
-export const redirectToRoute = (url: string) => ({
-  type: ActionName.RedirectToRoute,
-  payload: url,
-} as const);
-
+export const redirectToRoute = createAction(
+  ActionName.RedirectToRoute,
+  (url: string) => ({payload: url}),
+);
