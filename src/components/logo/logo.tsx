f8f77e95fb9a0daec22fa5b3efc8c0ac660fa15e
tyort <neotyo1986@gmail.com>
2022-10-17 17:19:31 +0300

    Компонент логотипа

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,23 @@
+import {Link} from 'react-router-dom';
+
+type LogoScreenProps = {
+  isLight: boolean;
+}
+
+function Logo({isLight = false}: LogoScreenProps): JSX.Element {
+  const isLightClass: string = isLight
+    ? 'logo__link logo__link--light'
+    : 'logo__link';
+
+  return (
+    <div className="logo">
+      <Link className={isLightClass} to="/">
+        <span className="logo__letter logo__letter--1">W</span>
+        <span className="logo__letter logo__letter--2">T</span>
+        <span className="logo__letter logo__letter--3">W</span>
+      </Link>
+    </div>
+  );
+}
+
+export default Logo;
