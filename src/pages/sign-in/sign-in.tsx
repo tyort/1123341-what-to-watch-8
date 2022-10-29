55b0f8b6395c2c46ec43f7b1d23d559edfd03899
tyort <neotyo1986@gmail.com>
2022-10-29 11:03:35 +0300

    Учимся подключать createRef

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,6 +1,17 @@
+import { FormEvent, createRef, RefObject } from 'react';
 import LogoScreen from '../../components/logo/logo';
 
 function SignInScreen(): JSX.Element {
+  // это просто объект, у которого есть свойство "current", в котором естьссылка на DOM-элемент;
+  const inputRef: RefObject<HTMLFormElement> = createRef();
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>): void => {
+    evt.preventDefault();
+
+    const tyort = new FormData(inputRef.current as HTMLFormElement).get('user-email');
+    console.log(tyort);
+  };
+
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -10,10 +21,21 @@ function SignInScreen(): JSX.Element {
       </header>
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form
+          onSubmit={handleSubmit}
+          ref={inputRef} // атрибут ref у React-элемента для связи с DOM-элементом
+          action="#"
+          className="sign-in__form"
+        >
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+              <input
+                className="sign-in__input"
+                type="email" placeholder="Email address"
+                name="user-email"
+                id="user-email"
+                defaultValue={'Что-там такое пиздатое'}
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
