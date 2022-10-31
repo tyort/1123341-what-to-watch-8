15b45ebce88a344f19ba3a4838b3eb80e38a4c6c
tyort <neotyo1986@gmail.com>
2022-10-31 11:18:31 +0300

    Воспроизводим превью при наведении курсора на карточку

--- src/pages/small-film-card/small-film-card.tsx
+++ src/pages/small-film-card/small-film-card.tsx
@@ -1,6 +1,6 @@
 import {Film} from '../../types/film';
 import { Link } from 'react-router-dom';
-import {MouseEvent} from 'react';
+import {MouseEvent, useEffect, useRef, useState} from 'react';
 
 type SmallFilmCardScreenProps = {
   id?: never;
@@ -9,18 +9,52 @@ type SmallFilmCardScreenProps = {
 }
 
 function SmallFilmCardScreen({film, getHoveredFilm}: SmallFilmCardScreenProps): JSX.Element {
+  const [mayPlay, setMayPlay] = useState<boolean>(false);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const videoElement = useRef<HTMLVideoElement | null>(null);
+
+  const handleCanplayVideo = () => {
+    setMayPlay(true);
+  };
+
+  useEffect(() => {
+    videoElement.current?.addEventListener('canplay', handleCanplayVideo);
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      videoElement.current?.removeEventListener('canplay', handleCanplayVideo);
+    };
+  }, [film]);
+
+  useEffect(() => {
+    if (mayPlay) {
+      isPlaying
+        ? videoElement.current?.play()
+        : videoElement.current?.load();
+    }
+  }, [isPlaying, mayPlay]);
+
   return (
     <article
       className="small-film-card catalog__films-card"
       onMouseOver={(evt: MouseEvent<HTMLElement>):void => {
         getHoveredFilm(film);
+        setIsPlaying(true);
       }}
       onMouseLeave={(evt: MouseEvent<HTMLElement>):void => {
         getHoveredFilm(null);
+        setIsPlaying(false);
       }}
     >
       <div className="small-film-card__image">
-        <img src={film.preview_image} alt={film.name} width="280" height="175" />
+        <video
+          style={{objectFit: 'cover', width: '100%', height: '100%'}}
+          poster={film.preview_image}
+          ref={videoElement}
+          loop
+          muted
+        >
+          <source src={film.preview_video_link} type="video/webm"/>
+        </video>
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${film.id}`}>{film.name}</Link>
