2cadf6c736bd3f535ae59b721a92eac1aa4b4fee
tyort <neotyo1986@gmail.com>
2022-11-29 15:21:05 +0300

    Подключаем Redux к React

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'; // Добавляем здесь в точке входа, в остальных компонентах можно не добавлять;
 import ReactDOM from 'react-dom/client'; // Добавляем здесь в точке входа, в остальных компонентах можно не добавлять;
 import App from './components/app/app';
+import {Provider} from 'react-redux'; // Provider - компонент. Мостик между React и Redux;
 import {films} from './mocks/films';
+import {store} from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -9,8 +11,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      films={films}
-    />
+    <Provider store = {store}>
+      <App
+        films={films}
+      />
+    </Provider>
   </React.StrictMode>,
 );
