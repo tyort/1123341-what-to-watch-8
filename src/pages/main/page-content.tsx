d52f240d6ab465f9ac713425fc5339657641b7e2
tyort <neotyo1986@gmail.com>
2022-11-29 23:55:40 +0300

    Применяем redux для сортировки фильмов по жанрам

--- src/pages/main/page-content.tsx
+++ src/pages/main/page-content.tsx
@@ -1,16 +1,19 @@
 import {Link} from 'react-router-dom';
-import { MouseEvent, useState } from 'react';
+import { MouseEvent } from 'react';
+import {useAppDispatch, useAppSelector} from '../../hooks';
 import LogoScreen from '../../components/logo/logo';
+import {getMoviesByGenre, changeGenre} from '../../store/action';
 import CatalogFilmsListScreen from '../catalog-films-list/catalog-films-list';
-import {Film} from '../../types/film';
 
-type MainChildScreenProps = {
-  films: Film[];
-}
+function MainChildScreen(): JSX.Element {
+  const {genre, genres, films} = useAppSelector((state) => state);
 
-function MainChildScreen({films}: MainChildScreenProps): JSX.Element {
-  const genres = Array.from(new Set(films.map((movie) => movie.genre)));
-  const [currentGenre, setCurrentGenre] = useState<string>('All genres');
+  const dispatch = useAppDispatch();
+
+  const onGenreChange = (selectedGenre: string) => {
+    dispatch(changeGenre({genre: selectedGenre}));
+    dispatch(getMoviesByGenre());
+  };
 
   return (
     <div className="page-content">
@@ -18,15 +21,15 @@ function MainChildScreen({films}: MainChildScreenProps): JSX.Element {
         <h2 className="catalog__title visually-hidden">Catalog</h2>
 
         <ul className="catalog__genres-list">
-          {['All genres', ...genres].map((genre) => (
+          {genres.map((item) => (
             <li
-              key={genre}
-              className={`catalog__genres-item ${currentGenre === genre ? 'catalog__genres-item--active' : ''}`}
+              key={item}
+              className={`catalog__genres-item ${genre === item ? 'catalog__genres-item--active' : ''}`}
               onClick={(evt: MouseEvent<HTMLLIElement>)=> {
-                setCurrentGenre(genre);
+                onGenreChange(item);
               }}
             >
-              <Link to="#" className="catalog__genres-link">{genre}</Link>
+              <Link to="#" className="catalog__genres-link">{item}</Link>
             </li>
           ))}
         </ul>
