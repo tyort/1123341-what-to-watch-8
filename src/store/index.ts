017fa91e311d5516478cb6f0e582bf5b53951a1b
tyort <neotyo1986@gmail.com>
2022-12-22 11:55:02 +0300

    Параметр "next"

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,10 +6,15 @@ import {createAPI} from '../services/api';
 export const api = createAPI();
 
 
-export const fuckYouSanta = (store: any) => (_nextDispath: any) => (action: any) => {
+export const fuckYouSanta = (store: any) => (next: any) => (action: any) => {
   console.log(`[${Date()}] — выполнено действие`);
   console.log(action);
   console.log('Текущее состояние:');
+  console.log(store.getState());
+
+  console.log('Выполняем действие…');
+  // С помощью "next" мы выполняем действие и передаём результат дальше, следующему middleware.
+  next(action);
   console.log(store.getState());
 };
 
