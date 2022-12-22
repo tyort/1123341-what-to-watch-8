f06d8040299ae4a030d974e34a184d8df91dad3c
tyort <neotyo1986@gmail.com>
2022-12-22 11:45:14 +0300

    Вот как под капотом выглядит middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,29 @@
+/* eslint-disable no-console */
 import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from './reducer';
 import {createAPI} from '../services/api';
 
 export const api = createAPI();
 
+
+// Читается неплохо, но вложенность
+// немного пугает
+export function fuckYouSanta(store: { getState: () => any }) {
+  return function (_nextDispath: any) {
+    return function (action: any) {
+      console.log(`[${Date()}] — выполнено действие`);
+      console.log(action); // объект
+      console.log('Текущее состояние:');
+      console.log(store.getState()); // объект состояния
+    };
+  };
+}
+
+// export const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: { extraArgument: api } }),
+// });
 export const store = configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      thunk: {
-        extraArgument: api,
-      },
-    }),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(fuckYouSanta),
 });
