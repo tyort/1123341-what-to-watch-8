c74e258fdeadfa71a320d1e5b90cd6bef2f21e9d
tyort <neotyo1986@gmail.com>
2022-12-23 22:48:02 +0300

    Диспатчим функцию типа AsyncThunkAction

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import App from './components/app/app';
 import {Provider} from 'react-redux'; // Provider - компонент. Мостик между React и Redux;
 import {films} from './mocks/films';
 import {store} from './store';
+import { fetchMoviesAction } from './store/api-actions';
+
+// Благодаря подключению "thunk" в "store" мы теперь можем диспатчить функции,
+// имеющих под капотом некую асинхронность;
+store.dispatch(fetchMoviesAction()); // Возвращает Promise
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
