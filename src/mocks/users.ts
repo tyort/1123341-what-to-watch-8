822d2abbc1ee88a96357e1ed371f61729daa2f8b
tyort <neotyo1986@gmail.com>
2022-12-26 18:47:21 +0300

    Создаем моки пользователей

---
+++ src/mocks/users.ts
@@ -0,0 +1,18 @@
+type User = {
+  email: string;
+  password: string;
+}
+export const users: User[] = [
+  {
+    email: 'gomarjoba@mail.ru',
+    password: '111',
+  },
+  {
+    email: 'chic@mail.ru',
+    password: '222',
+  },
+  {
+    email: 'lana@mail.ru',
+    password: '333',
+  },
+];
