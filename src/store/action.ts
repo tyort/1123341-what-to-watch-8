2a74efefa05c1dc34f0a39995ca76978a44efb64
tyort <neotyo1986@gmail.com>
2023-01-07 13:47:56 +0300

    Синхронное действие по загрузке комментариев

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 import { Film } from '../types/film';
+import { Comment } from '../types/comment';
 import { User } from '../mocks/users';
 
 // Действие - это самый обычный объект;
@@ -9,6 +10,7 @@ export const loadMovies = createAction<{
   genre: string;
   showButton: boolean;
 }>('loadingData/loadFilms');
+export const loadComments = createAction<Comment[]>('loadingData/loadComments');
 export const setAuthorizationStatus = createAction<User | 'NO_AUTH' | 'UNKNOWN'>('authorization/setAuthorizationStatus');
 export const showErrorMessage = createAction<string | undefined>('error/showErrorMessage');
 export const hideErrorMessage = createAction('error/hideErrorMessage');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,6 +1,7 @@
 import {createReducer} from '@reduxjs/toolkit';
 import { Film } from '../types/film';
-import {hideErrorMessage, loadMovies, setAuthorizationStatus, showErrorMessage} from './action';
+import { Comment } from '../types/comment';
+import {hideErrorMessage, loadMovies, loadComments, setAuthorizationStatus, showErrorMessage} from './action';
 
 const FILMS_COUNT_DIVIDER = 4;
 
@@ -12,6 +13,7 @@ const initialState: {
   showButton: boolean;
   authorizationStatus: 'AUTH' | 'NO_AUTH' | 'UNKNOWN';
   errorMessage: string | undefined;
+  currentComments: Comment[];
 } = {
   genre: 'All genres',
   films: [],
@@ -19,13 +21,17 @@ const initialState: {
   filmsCount: FILMS_COUNT_DIVIDER,
   showButton: true,
   authorizationStatus: 'UNKNOWN',
-  errorMessage: undefined
+  errorMessage: undefined,
+  currentComments: []
 };
 
 // Редюсеры определяют, как состояние приложения изменяется в ответ на экшены, отправленные в стор.
 // Помните, что экшены только описывают, _что произошло, но не описывают, как изменяется состояние приложения.
 const reducer = createReducer(initialState, (builder) => {
   builder
+    .addCase(loadComments, (state, action) => {
+      state.currentComments = action.payload;
+    })
     .addCase(loadMovies, (state, action) => {
       const {moviesForView, genres, genre, showButton} = action.payload;
       state.films = moviesForView;
